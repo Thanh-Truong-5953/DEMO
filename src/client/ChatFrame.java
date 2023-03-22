feb0c8ace02275f916b885e0603257e96c9c2199
TungDragon <tungdragon113@gmail.com>
2023-03-22 22:04:30 +0700

    ThanhTung commit giao dien login, chat

--- src/client/ChatFrame.java
+++ src/client/ChatFrame.java
@@ -5,10 +5,144 @@
  */
 package client;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.HashMap;
+import javax.sound.midi.Receiver;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+
 /**
  *
  * @author My Laptop
  */
-public class ChatFrame {
+public class ChatFrame  extends JFrame{
+    private JButton btnFile;
+	private JButton btnSend;
+	private JScrollPane chatPanel;
+	private JLabel lbReceiver = new JLabel(" ");
+	private JPanel contentPane;
+	private JTextField txtMessage;
+	private JTextPane chatWindow;
+	JComboBox<String> onlineUsers = new JComboBox<String>();
+	
+	private String username;
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	
+	private HashMap<String, JTextPane> chatWindows = new HashMap<String, JTextPane>();
+	
+	Thread receiver;
+        public ChatFrame(String username, DataInputStream dis, DataOutputStream dos) {
+            setTitle("CLG CHAT");	
+		this.username = username;
+		this.dis = dis;
+		this.dos = dos;
+//		receiver = new Thread(new Receiver(dis));
+		receiver.start();
+		
+		setDefaultLookAndFeelDecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 586, 450);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(new Color(230, 240, 247));
+		setContentPane(contentPane);
+		
+		JPanel header = new JPanel();
+		header.setBackground(new Color(160, 190, 223));
+		
+		txtMessage = new JTextField();
+		txtMessage.setEnabled(false);
+		txtMessage.setColumns(10);
+		
+		btnSend = new JButton("");
+		btnSend.setEnabled(false);
+		btnSend.setIcon(new ImageIcon("data\\icon\\component\\send.png"));
+		
+		chatPanel = new JScrollPane();
+		chatPanel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		JPanel leftPanel = new JPanel();
+		leftPanel.setBackground(new Color(230, 240, 247));
+		
+		btnFile = new JButton("");
+                JLabel userImage = new JLabel(new ImageIcon("data\\icon\\component\\user.png"));
+                JPanel panel = new JPanel();
+		panel.setBackground(new Color(230,240,247));
+		JLabel lblNewLabel_1 = new JLabel("CHAT WITH");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		GroupLayout gl_leftPanel = new GroupLayout(leftPanel);
+		gl_leftPanel.setHorizontalGroup(
+			gl_leftPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_leftPanel.createSequentialGroup()
+					.addGap(25)
+					.addComponent(userImage, GroupLayout.DEFAULT_SIZE, 71, Short.MAX_VALUE)
+					.addGap(25))
+				.addGroup(gl_leftPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(gl_leftPanel.createSequentialGroup()
+					.addGap(28)
+					.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(29))
+				.addGroup(Alignment.TRAILING, gl_leftPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(onlineUsers, 0, 101, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+                gl_leftPanel.setVerticalGroup(
+			gl_leftPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_leftPanel.createSequentialGroup()
+					.addGap(5)
+					.addComponent(userImage)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+					.addGap(41)
+					.addComponent(lblNewLabel_1)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(onlineUsers, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(104, Short.MAX_VALUE))
+		);
+		
+		JLabel lbUsername = new JLabel(this.username);
+		lbUsername.setFont(new Font("Arial", Font.BOLD, 15));
+		panel.add(lbUsername);
+		leftPanel.setLayout(gl_leftPanel);
+		
+		JLabel headerContent = new JLabel("CLG CHAT");
+		headerContent.setFont(new Font("Poor Richard", Font.BOLD, 24));
+		header.add(headerContent);
+		
+		JPanel usernamePanel = new JPanel();
+		usernamePanel.setBackground(new Color(230,240,247));
+		chatPanel.setColumnHeaderView(usernamePanel);
+		
+		lbReceiver.setFont(new Font("Arial", Font.BOLD, 16));
+		usernamePanel.add(lbReceiver);
+		
+		chatWindows.put(" ", new JTextPane());
+		chatWindow = chatWindows.get(" ");
+		chatWindow.setFont(new Font("Arial", Font.PLAIN, 14));
+		chatWindow.setEditable(false);
+		
+		chatPanel.setViewportView(chatWindow);
+//		contentPane.setLayout(gl_contentPane);
+        }
     
 }
--- src/client/LoginFrame.java
+++ src/client/LoginFrame.java
@@ -4,11 +4,136 @@
  * and open the template in the editor.
  */
 package client;
+    
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
  * @author My Laptop
  */
-public class LoginFrame {
-    
+   public class LoginFrame extends JFrame{
+       private JPanel contentPane;
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
+
+	private String host = "localhost";
+	private int port = 9999;
+	private Socket socket;
+	
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	
+	private String username;
+        
+    public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+    public LoginFrame (){
+        setTitle("CLG CHAT");
+		
+		setDefaultLookAndFeelDecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(230, 240, 247));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel headerPanel = new JPanel();
+		headerPanel.setBackground(new Color(160,190,223));
+		
+		JLabel lbUsername = new JLabel("Username");
+		lbUsername.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		JLabel lbPassword = new JLabel("Password");
+		lbPassword.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		txtUsername = new JTextField();
+		txtUsername.setColumns(10);
+		
+		txtPassword = new JPasswordField();
+                JPanel buttons = new JPanel();
+		buttons.setBackground(new Color(230, 240, 247));
+		JPanel notificationContainer = new JPanel();
+		notificationContainer.setBackground(new Color(230, 240, 247));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(headerPanel, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(69)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lbPassword)
+							.addGap(18)
+							.addComponent(txtPassword, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lbUsername)
+							.addGap(18)
+							.addComponent(txtUsername, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)))
+					.addGap(81))
+				.addComponent(notificationContainer, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
+				.addComponent(buttons, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(headerPanel, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+					.addGap(33)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbUsername)
+						.addComponent(txtUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(30)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lbPassword))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(notificationContainer, GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(buttons, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+					.addGap(22))
+		);
+		
+		JLabel notification = new JLabel("");
+		notification.setForeground(Color.RED);
+		notification.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		notificationContainer.add(notification);
+		
+		JButton login = new JButton("Log in");
+		JButton signup = new JButton("Sign up");
+                login.setEnabled(false);
+                signup.setEnabled(false);
+                
+                JLabel headerContent = new JLabel("LOGIN");
+		headerContent.setFont(new Font("Poor Richard", Font.BOLD, 24));
+		headerPanel.add(headerContent);
+		contentPane.setLayout(gl_contentPane);
+    }
 }
+
+
